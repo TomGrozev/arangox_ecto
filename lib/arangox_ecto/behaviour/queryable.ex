252d24213a20543cde87aee04242619d92bf106b
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2023-04-09 00:41:21 +1000

    Fix for query write op on delete

--- lib/arangox_ecto/behaviour/queryable.ex
+++ lib/arangox_ecto/behaviour/queryable.ex
@@ -46,7 +46,7 @@ defmodule ArangoXEcto.Behaviour.Queryable do
       }
     })
 
-    is_write_operation = String.match?(query, ~r/(update|delete) .+/i)
+    is_write_operation = String.match?(query, ~r/(update|remove) .+/i)
     is_static = Keyword.get(repo.config(), :static, false)
 
     {run_query, options} = process_sources(conn, sources, is_static, is_write_operation)
